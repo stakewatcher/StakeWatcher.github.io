886c8499eedb6cb51d18302dcd6eb51a3b2c69f4
stakewatcher <stakewatcherbot@gmail.com>
2022-04-13 20:34:47 +0200

    20220413

---
+++ js/demo/datatables-dropdown.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    var table = $('#dataTable').DataTable({
+        orderCellsTop: true,
+        dom: 'Bfrtip',
+        initComplete: function() {
+            this.api().columns().every(function() {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo($("#dataTable thead tr:eq(1) th").eq(column.index()).empty())
+                    .on('change', function() {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search(val ? '^' + val + '$' : '', true, false)
+                            .draw();
+                    });
+
+                column.data().unique().sort().each(function(d, j) {
+                    select.append('<option value="' + d + '">' + d + '</option>');
+                });
+            });
+        }
+    });
+});
---
+++ js/demo/datatables-full.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    // Setup - add a text input to each footer cell
+    $('#dataTableFull thead tr')
+        .clone(true)
+        .addClass('filters')
+        .appendTo('#dataTableFull thead');
+ 
+    var table = $('#dataTableFull').DataTable({
+        "pageLength": 10,
+        orderCellsTop: true,
+        fixedHeader: true,
+        initComplete: function () {
+            var api = this.api();
+ 
+            // For each column
+            api
+                .columns()
+                .eq(0)
+                .each(function (colIdx) {
+                    // Set the header cell to contain the input element
+                    var cell = $('.filters th').eq(
+                        $(api.column(colIdx).header()).index()
+                    );
+                    var title = $(cell).text();
+                    $(cell).html('<input type="text" placeholder="' + title + '" />');
+ 
+                    // On every keypress in this input
+                    $(
+                        'input',
+                        $('.filters th').eq($(api.column(colIdx).header()).index())
+                    )
+                        .off('keyup change')
+                        .on('keyup change', function (e) {
+                            e.stopPropagation();
+ 
+                            // Get the search value
+                            $(this).attr('title', $(this).val());
+                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
+ 
+                            var cursorPosition = this.selectionStart;
+                            // Search the column for that value
+                            api
+                                .column(colIdx)
+                                .search(
+                                    this.value != ''
+                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
+                                        : '',
+                                    this.value != '',
+                                    this.value == ''
+                                )
+                                .draw();
+ 
+                            $(this)
+                                .focus()[0]
+                                .setSelectionRange(cursorPosition, cursorPosition);
+                        });
+                });
+        },
+    });
+});
